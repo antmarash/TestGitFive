6de72b29dc632bd9db8b1c5ad0a93cb6986c03e3
Antranig Marashlian <antmarash@gmail.com>
2025-02-11 09:54:04 +0300

    Create MdlUser

---
+++ MdlUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitFive
+{
+    public class MdlUser
+    {
+        public int ID { get; set; }
+        public string DName { get; set; }
+    }
+}
